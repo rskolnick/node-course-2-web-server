644600082e2ad0a27c62103e56b48c06eacbc1e7
Ryan Skolnick <ryan@aveholearning.com>
2018-11-14 10:55:27 -0500

    Setup start script and port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
+
 
 var app = express();
 
@@ -56,6 +58,6 @@ app.get('/bad', (request, response) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on Port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on Port ${port}`);
 });
